c65a477e7b2de3eb45f63291a7eddf1c3f14a13f
Yoseph Radding <raddingy@lykeme.com>
2015-02-28 07:54:33 -0600

    tyfghhgf

--- Assets/scripts/CharacterController.cs
+++ Assets/scripts/CharacterController.cs
@@ -4,17 +4,25 @@ using System.Collections;
 public class CharacterController : MonoBehaviour {
 
 	public float speed = 10.0f;
+	public float rotSpeed = 1.0f;
 	public GameObject ammo;
 	bool canFire;
 	public float fireRate;
+	public GameObject myo;
+	private ThalmicMyo thalmicMyo;
 
 	// Use this for initialization
 	void Start () {
-	
+		MyoManager.Initialize ();
+		MyoManager.PresentPairing (); 
+//		thalmicMyo = myo.GetComponent<ThalmicMyo> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		transform.Translate (transform.forward * speed * Time.deltaTime);
+		Vector3 newEulerAngles = MyoManager.GetQuaternion ().eulerAngles;
+//		newEulerAngles = thalmicMyo.gyroscope;
+		transform.Rotate (newEulerAngles);
 	}
 }

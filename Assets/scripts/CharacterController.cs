97331d80d2a186fe26b604fe8a7b67c9eaac276a
Yoseph Radding <raddingy@lykeme.com>
2015-02-28 00:27:51 -0600

    Added scripts and shit.

---
+++ Assets/scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public GameObject ammo;
+	bool canFire;
+	public float fireRate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}

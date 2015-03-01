c3e94e8aca12c055ba4e5b8d678076e80cd1c433
Yoseph Radding <raddingy@lykeme.com>
2015-03-01 04:35:16 -0600

    Things

--- Assets/scripts/Lazer.cs
+++ Assets/scripts/Lazer.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Lazer : MonoBehaviour {
 
 	bool move = true;
+	float timer = 2.0f;
 	// Use this for initialization
 	void Start () {
 
@@ -13,6 +14,9 @@ public class Lazer : MonoBehaviour {
 	void Update () {
 			if (move)
 				transform.position += transform.forward * 100 * Time.deltaTime;
+		if (timer <= 0.0f)
+						Destroy (this.gameObject);
+		timer -= Time.deltaTime;
 	}
 
 	public void moved(){
